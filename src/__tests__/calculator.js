296988434e436b923c7af32c8dfe0ff0b9be2777
Randel Ramirez <randelramirez1@gmail.com>
2022-01-15 17:23:22 +0800

    add test

--- src/__tests__/calculator.js
+++ src/__tests__/calculator.js
@@ -1,7 +1,18 @@
 import React from 'react'
-import {render} from '@testing-library/react'
+import {fireEvent, render} from '@testing-library/react'
 import Calculator from '../calculator'
 
 test('renders', () => {
   render(<Calculator />)
+})
+
+test('the clear button swithces from AC to C when there is an entry', () => {
+  const {getByText} = render(<Calculator />)
+  const clearButton = getByText('AC')
+
+  fireEvent.click(getByText(/3/))
+  expect(clearButton.textContent).toBe('C')
+
+  fireEvent.click(clearButton)
+  expect(clearButton.textContent).toBe('C')
 })
